6c031456ac0c9d54b98495f5eea600b4e9a84c7b
Bhanuka Pawan Athukorala <bhanuka_pawan@yahoo.com>
2026-02-16 23:40:50 +0530

    Commit uncommited files related to user management CRUD

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,29 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const cors = require("cors");
-const connectDB = require("./src/config/db");
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./src/config/db.js";
+import userRoutes from "./src/routes/userRoutes.js";
 
 dotenv.config();
 connectDB();
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// Test route
 app.get("/", (req, res) => {
-  res.send("PeriodPal API is running...");
+    res.send("PeriodPal API is running...");
 });
 
+// User Routes
+app.use("/api/users", userRoutes);
+
+// Set Port
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () =>
-  console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 );
