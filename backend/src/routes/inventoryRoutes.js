be66a3160c6d1eb6b3d08a52a68f8f470b14d457
Nethmi Hewasinghe <nethmihewasinghe02@gmail.com>
2026-02-25 16:47:47 +0530

    update changes

--- backend/src/routes/inventoryRoutes.js
+++ backend/src/routes/inventoryRoutes.js
@@ -3,8 +3,7 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/inventoryController");
 
-// ✅ TEMP: auth removed ONLY for testing in Postman
-// const { protect, authorize } = require("../middleware/authMiddleware");
+
 
 // third-party feature
 router.get("/nearby", controller.reverseGeocodeCenter);
