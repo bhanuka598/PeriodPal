9ff5f4190b759b44c3f0cc7bac156f6dfced4702
Nethmi Hewasinghe <nethmihewasinghe02@gmail.com>
2026-02-25 16:22:36 +0530

    connect inventory routes to server

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./src/config/db");
 
+// routes
+const inventoryRoutes = require("./src/routes/inventoryRoutes");
+
 dotenv.config();
 connectDB();
 
@@ -15,8 +18,9 @@ app.get("/", (req, res) => {
   res.send("PeriodPal API is running...");
 });
 
+// Inventory
+app.use("/api/inventory", inventoryRoutes);
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () =>
-  console.log(`Server running on port ${PORT}`)
-);
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
