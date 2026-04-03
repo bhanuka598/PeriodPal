27fbec2a2cd3812f8f86da4fb7d9ebf5dbe749c2
Nethmi Hewasinghe <nethmihewasinghe02@gmail.com>
2026-04-03 16:49:35 +0530

    update backend server.js with PATCH method

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors({
   origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT' , 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
