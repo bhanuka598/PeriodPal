4eb474197ee5588a0f5a892463255530d7ad67f1
Bhanuka Pawan Athukorala <bhanuka_pawan@yahoo.com>
2026-04-26 09:43:52 +0530

    Revert "update"
    This reverts commit ad6d831d63c78ef66cc599ac9f9f52894a09556f.

--- backend/src/services/emailService.js
+++ backend/src/services/emailService.js
@@ -1,17 +1,10 @@
-const dns = require('dns');
 const nodemailer = require('nodemailer');
-const { smtpLookupIPv4 } = require('../utils/smtpLookup');
-
-if (typeof dns.setDefaultResultOrder === 'function') {
-  dns.setDefaultResultOrder('ipv4first');
-}
 
 // Create reusable transporter object using SMTP transport
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 587,
   secure: false,
-  lookup: smtpLookupIPv4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
